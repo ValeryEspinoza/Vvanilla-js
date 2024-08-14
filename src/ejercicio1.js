0f5398e155b39946695881fcc8e829abc63b06bb
ValeryEspinoza <vespinoza@fwdcostarica.com>
2024-08-14 10:53:51 -0600

    Ejercicio 2

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,23 @@
+
+
 let contador = {
   valor: 0,
   siguiente: function () {
-    // Inserte el código aquí
+   return this.valor++
   },
 };
-
+ 
 export { contador }
+
+
+
+
+
+
+
+
+
+/*## Ejercicio 1
+
+1. Realice el ejercicio en el archivo: ejercicio1.js
+2. Utilizando el keyword "this", haga que la funcion siguiente incremenete el valor en 1 y que lo retorne.*/
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,26 @@
+
+
+
 class Contador {
-  // Inserte el código aquí
+  
+  valor=0;
+
+  siguiente(){
+
+    return this.valor++
+
+  }
+
 }
 
-// Inserte el código aquí
+const instancia= new Contador()
 
 export { instancia }
+
+/*## Ejercicio 2
+
+1. Realice el ejercicio en el archivo: ejercicio1.js
+2. Cree una clase con:
+   * Una propiedad valor inicializada en 0
+   * Un método siguiente que incremenente el valor en 1.
+3. Defina una variable llamada "instancia" que inicialice una instancia de la clase.*/
