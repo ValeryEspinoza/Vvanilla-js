bc2b84ee6966d15bc90b744252f9b4bada21261d
ValeryEspinoza <vespinoza@fwdcostarica.com>
2024-08-14 08:10:53 -0600

    Ejercicio 1

---
+++ dist/index.579125c3.js
@@ -0,0 +1,15 @@
+class Auto {
+    constructor(marca, modelo, year){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.year = year;
+    }
+    mostrarAuto() {
+        return `Este Vehiculo es ${this.marca} ${this.modelo} ${this.year}`;
+    }
+}
+const miCarroToyota = new Auto(`Toyota`, `Corolla`, 2024);
+const miCarroHonda = new Auto(`Honda`, `Elenta`, 2024);
+console.log(miCarroToyota.mostrarAuto());
+
+//# sourceMappingURL=index.579125c3.js.map
